e5dceafe52c79aa8307b628cfa9e9c954f17042a
Luis Diaz <41093870+LDiazN@users.noreply.github.com>
2025-02-09 21:11:14 +0100

    Add state changing logic

--- Assets/Scripts/AI/EnemyStates.cs
+++ Assets/Scripts/AI/EnemyStates.cs
@@ -4,7 +4,26 @@ namespace AI
     {
         public void Execute(EnemyAgent agent)
         {
+            var nextState = GetNextState(agent);
+            if (nextState != EnemyAgent.State.Patrol)
+            {
+                agent.ChangeState(nextState);
+                return;
+            }
+                
             
+        }
+
+        private EnemyAgent.State GetNextState(EnemyAgent agent)
+        {
+            var perceptions = agent.GetPerceptions();
+            if (perceptions.canSeePlayer)
+                return EnemyAgent.State.Chase;
+
+            if (perceptions.canHearPlayer)
+                return EnemyAgent.State.Alert;
+
+            return EnemyAgent.State.Patrol;
         }
     }
     
@@ -12,7 +31,24 @@ namespace AI
     {
         public void Execute(EnemyAgent agent)
         {
+            var nextState = GetNextState(agent);
+            if (nextState != EnemyAgent.State.Alert)
+            {
+                agent.ChangeState(nextState);
+                return;
+            }
             
+        }
+        
+        private EnemyAgent.State GetNextState(EnemyAgent agent)
+        {
+            var perceptions = agent.GetPerceptions();
+            if (!perceptions.canHearPlayer && !perceptions.canSeePlayer)
+                return EnemyAgent.State.Patrol;
+            if (perceptions.canSeePlayer)
+                return EnemyAgent.State.Chase;
+
+            return EnemyAgent.State.Alert;
         }
     }
     
@@ -20,7 +56,22 @@ namespace AI
     {
         public void Execute(EnemyAgent agent)
         {
+            var nextState = GetNextState(agent);
+            if (nextState != EnemyAgent.State.Alert)
+            {
+                agent.ChangeState(nextState);
+                return;
+            }
             
+        }
+        
+        private EnemyAgent.State GetNextState(EnemyAgent agent)
+        {
+            var perceptions = agent.GetPerceptions();
+            if (!perceptions.canSeePlayer && !perceptions.canHearPlayer)
+                return EnemyAgent.State.Alert;
+
+            return EnemyAgent.State.Chase;
         }
     }
 }
